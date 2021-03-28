dbca334346458c31e4f9ad39ecff3f44cd469fbc
Massimiliano Pippi <mpippi@gmail.com>
2021-03-28 17:04:13 +0200

    add option to specify where to find the config file

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 func main() {
 	// Define and parse command args
 	verbosity := pflag.IntP("verbose", "v", 1, "set verbosity level: 0 silent, 1 normal, 2 debug")
+	configPath := pflag.StringP("config", "c", "", "path to config file")
 	help := pflag.BoolP("help", "h", false, "print args help")
 	pflag.Parse()
 
@@ -30,7 +31,7 @@ func main() {
 	output.Init(*verbosity)
 
 	// Bootstrap config, this has to be called first
-	initConfig()
+	initConfig(configPath)
 
 	// Define the available output plugins
 	plugins := map[string]plugins.Plugin{
@@ -51,7 +52,7 @@ func main() {
 	os.Exit(0)
 }
 
-func initConfig() {
+func initConfig(configPath *string) {
 	// Defaults
 	viper.SetDefault("port", "3060")
 
@@ -63,6 +64,9 @@ func initConfig() {
 	viper.SetConfigName("threadle.yaml")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
+	if *configPath != "" {
+		viper.AddConfigPath(*configPath)
+	}
 	err := viper.ReadInConfig()
 	if err != nil {
 		output.FATAL.Fatalf("Fatal error: %s", err)
